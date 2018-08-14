fda454b06bc553e11f86f5ee1acc75cc70ad69c7
Lee Briggs <coffeegunshow@gmail.com>
2018-08-14 15:35:28 +1000

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Personal site
-
-* WIP 😃
-* Hosted at [theleebriggs.github.io](https://theleebriggs.github.io/)
-* Bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app)
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'normalize.css'
-import './index.css'
-import Resume from './Resume'
-import registerServiceWorker from './registerServiceWorker'
-
-ReactDOM.render(<Resume />, document.getElementById('root'))
-registerServiceWorker()
