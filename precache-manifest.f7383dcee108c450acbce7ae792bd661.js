a776e5a548b85e096419ab56f18bfbe7df8a7902
Lee Briggs <coffeegunshow@gmail.com>
2020-06-09 08:34:04 +1000

    Updates

---
+++ precache-manifest.f7383dcee108c450acbce7ae792bd661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33b1601f6faf08f81f66",
+    "url": "/static/css/main.9d89d017.chunk.css"
+  },
+  {
+    "revision": "33b1601f6faf08f81f66",
+    "url": "/static/js/main.9c125989.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "baea647953afd1d880b5",
+    "url": "/static/css/2.8e479cc1.chunk.css"
+  },
+  {
+    "revision": "baea647953afd1d880b5",
+    "url": "/static/js/2.d905bd39.chunk.js"
+  },
+  {
+    "revision": "f823914428d416193d210c20c8f6524a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e96e200529f202f9ac63e4414cc63d3c.js"
+  "/precache-manifest.f7383dcee108c450acbce7ae792bd661.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
